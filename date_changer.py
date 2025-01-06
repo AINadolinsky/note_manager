70f8900d4ebfa22b67e11cdf2fa4b13631c070ea
andrey.nadolinsky@gmail.com <andrey.nadolinsky@gmail.com>
2025-01-07 00:19:37 +0300

    data_changer

---
+++ date_changer.py
@@ -0,0 +1,16 @@
+# Creating variables
+username = 'Andrey' # Переменная для имени пользователя
+title = 'A note' # Переменная для заголовка заметки
+content = 'Description of the note' # Переменная для описания заметки
+status = 'Note status' # Переменная для статуса заметки
+created_date = '01/01/2025' # Дата начала заметки
+issue_date = '01/04/2025' # Дедлайн
+
+# For run
+print('Имя пользователя: '+ username + ", username")
+print('Заголовок заметки: '+ title + ', ' + 'title')
+print('Описание заметки: '+ content + ', ' + 'content')
+print('Статус заметки: '+ status + ', ' + 'status')
+print('Дата создания: '+ created_date[0:5] + ', ' + 'created_date')
+print('Дата завершения: '+ issue_date[0:5] + ', ' + 'issue_date')
+
